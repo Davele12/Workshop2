aab42b968e0d5d5226271371fb5147c3714f2c4e
Davele12 <126210636+Davele12@users.noreply.github.com>
2024-04-16 21:37:52 -0500

    Updated workshop

--- DavidWorkshop2/dags/dag_test.py
+++ DavidWorkshop2/dags/dag_test.py
@@ -1,5 +1,3 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql import text
 from sqlalchemy import create_engine
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -26,6 +24,39 @@ def etl_process(sql_query):
     transformed_df = transform_data(df)
     return transformed_df
 
+def read_csv():
+    df_csv = pd.read_csv('/opt/airflow/dags/files/spotify_dataset.csv')
+    if df_csv is not None:
+        return df_csv
+    else:
+        raise ValueError("CSV data could not be loaded.")
+
+def transform_csv():
+    df = pd.read_csv('/opt/airflow/dags/files/spotify_dataset.csv')
+    df.drop('Unnamed: 0', axis=1, inplace=True)
+ 
+
+def merge_dataframes(df1, df2, key1, key2):
+    merged_df = pd.merge(df1, df2, left_on=key1, right_on=key2, how='inner')
+    
+    return merged_df
+
+def merge_task(**kwargs):
+    # Obtiene los DataFrames de XCom
+    df_db = kwargs['ti'].xcom_pull(task_ids='read_db_task')
+    df_csv = kwargs['ti'].xcom_pull(task_ids='read_csv_task')
+    
+    # Verifica que ambos DataFrames no sean None
+    if df_db is None or df_csv is None:
+        raise ValueError("One or more input DataFrames are None.")
+    
+    # Continúa con el proceso de merge si ambos DataFrames están presentes
+    merged_df = merge_dataframes(df_csv, df_db, 'artists', 'artist')
+    # ...
+    return merged_df
+
+
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -37,7 +68,6 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-# Define el DAG
 dag = DAG(
     'ETL_DAG',
     default_args=default_args,
@@ -45,13 +75,13 @@ dag = DAG(
     schedule_interval=timedelta(days=1),
 )
 
-# Define las tareas utilizando PythonOperator
 read_db_task = PythonOperator(
     task_id='read_db',
     python_callable=query_data,
     op_kwargs={'sql_query': 'SELECT * FROM "grammyAwards_Airflow" LIMIT 5;'},
     dag=dag,
 )
+
 transform_db_task = PythonOperator(
     task_id='etl_process',
     python_callable=etl_process,
@@ -59,6 +89,37 @@ transform_db_task = PythonOperator(
     dag=dag,
 )
 
-# Define la secuencia de tareas
+read_csv_task = PythonOperator(
+    task_id='read_csv',
+    python_callable=read_csv,
+    dag=dag,
+)
+
+transform_csv_task = PythonOperator(
+    task_id='transform_csv',
+    python_callable=transform_csv,
+    dag=dag,
+)
+
+merge = PythonOperator(
+    task_id='merge_data',
+    python_callable=merge_task,
+    dag=dag,
+)
+
+#load_task = PythonOperator(
+#    task_id='load_data',
+#    python_callable=load_data,  # Asumiendo que tienes una función definida para esto
+#    op_kwargs={'table_name': 'merged_data'},  # O los argumentos apropiados
+#    dag=dag,
+#)
+
+read_csv_task >> transform_csv_task
 read_db_task >> transform_db_task
+
+# La tarea de merge debería ejecutarse después de ambas transformaciones
+[transform_csv_task, transform_db_task] >> merge
+
+# La tarea de carga se ejecuta después del merge
+#merge >> load_task
 
